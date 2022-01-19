ee8730507958d71ac6a899a1232c32b28c06cc41
w7 <yushimitsu@ubuntu.ubuntu-domain>
2022-01-18 23:08:19 -0300

    changes news

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    alert('hola')
-});
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+    fulImg.classList.add("modalOpen")
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
+
+document.addEventListener("click", (event) => {
+    console.log()
+    if(event.target.classList.contains("img")){
+        return true
+    }
+    if(fulImg.classList.contains("modalOpen")){
+        fulImgBox.style.display = "none";
+        fulImg.classList.remove("modalOpen")
+        console.log("pipo")
+    }
+    console.log("caca")
+})
